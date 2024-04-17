6b10863626fd0d512f780be54288b37ef6e517a1
mkjega <103827345+mkjega@users.noreply.github.com>
2024-04-17 18:42:00 +0530

    Update MeetingModel.tsx

--- src/components/MeetingModel.tsx
+++ src/components/MeetingModel.tsx
@@ -26,7 +26,7 @@ interface MeetingModelProps {
 const MeetingModel = ({isOpen, onClose, title, className, children, handleClick, buttonText, image, buttonIcon}: MeetingModelProps) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogTrigger>Open</DialogTrigger>
+        
         <DialogContent className='flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
             <div className='flex flex-col gap-6'>
                 {image && (
